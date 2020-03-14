5d81772cc3388c9bc3e61bc77bd0c6369384289c
KAT <katreadwell1@gmail.com>
2020-03-14 14:30:54 -0700

    added button to retry game but messed up question layout by making them display horizontally (removed p tags)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //declare global variables, set up trivia questions and store in trivia object
-var timer = 5;
+var timer = 10;
 var timerOn = false;
 var intervalId;
 var correct = 0;
@@ -12,6 +12,11 @@ var trivia = {
         q3: "What is the device trainers use to keep a record of their Pokemon encounters?",
         q4: "If you need to buy supplies in the Pokemon world, where do you go?",
         q5: "If you are facing a Magmar in battle, which of these attacks should your Pokemon use?",
+        q6: "If you need to revive your fainted Pokemon to full health, where do you go?",
+        q7: "What type of attacks are Normal type Pokemon immune to?",
+        q8: "What type of Pokemon does Gym Leader Erika specialize in?",
+        q9: "What type of Pokemon is Mewtwo?",
+        q10: "What type of attach are Flying type Pokemon immune to?",
     },
     options:{
         q1: ["Jolteon", "Electivire", "Meowstic", "Raichu"],
@@ -19,6 +24,11 @@ var trivia = {
         q3: ["Pokecounter", "Pokefinder", "Pokedex", "Pokephone"],
         q4: ["Pokemon Center", "Gym", "Poke Mart", "Poke Dep"],
         q5: ["Hyper Beam", "Solar Beam", "Earthquake", "Splash"],
+        q6: ["Mount Fuji", "Pokemon Mansion", "Pokemon Center", "Gym"],
+        q7: ["Fighting", "Ghost", "Dark", "Fire"],
+        q8: ["Earth", "Water", "Electric", "Grass"],
+        q9: ["Fighting", "Psychic", "Fairy", "Dark"],
+        q10: ["Normal", "Dragon", "Ground", "Water"],
     },
     answers: {
         q1: "Raichu",
@@ -26,6 +36,11 @@ var trivia = {
         q3: "Pokedex",
         q4: "Poke Mart",
         q5: "Earthquake",
+        q6: "Pokemon Center",
+        q7: "Ghost",
+        q8: "Grass",
+        q9: "Psychic",
+        q10: "Ground",
     },
     choices:{
         q1: 0,
@@ -33,6 +48,11 @@ var trivia = {
         q3: 0,
         q4: 0,
         q5: 0,
+        q6: 0,
+        q7: 0,
+        q8: 0, 
+        q9: 0,
+        q10: 0,
     }
 }
 
@@ -41,6 +61,7 @@ var trivia = {
 $(document).ready(function(){
 $('.start').show();
 $('.game').hide();
+$('#retry').hide();
 });
 
 //what happens after you click start
@@ -48,6 +69,7 @@ $('#start').on('click', function(){
 showQuestions(); 
 $('.start').hide();
 $('.game').show();
+$('#retry').hide();
 });
 
 
@@ -73,7 +95,7 @@ for (var triviaKey in trivia.questions){
     var options = trivia.options[triviaKey];  
     for (var i = 0; i < options.length; i++){
         var choice = options[i];  
-        $(question).append(`<p><input type="radio" class="answer" value="${choice}" name="${triviaKey}">${choice}</p>`)
+        $(question).append(`<input type="radio" class="answer" value="${choice}" name="${triviaKey}">${choice}`)
     }
         $('.trivia').append(question);
 }
@@ -89,36 +111,19 @@ startTimer();
 // loop over selected input and then compare if it is correct, increment accordingly
 function checkQuestions(){
     for (var triviaKey in trivia.answers){
-        // var answers = $(`<div><p>${trivia.answers[triviaKey]}</p>`);
-        // var choices = $(`<div><p>${trivia.choices[triviaKey]}</p>`);
-        // console.log(trivia.answers[triviaKey]);
-        // console.log(trivia.choices[triviaKey]);
-
         if (trivia.answers[triviaKey] === trivia.choices[triviaKey]){
             console.log("user got " + triviaKey + 'right');
             correct++;
         } else if (trivia.choices[triviaKey] === 0){
-            console.log("user got " + triviaKey + 'wrong');
+            console.log("user got " + triviaKey + 'unanswered');
             unanswered++;
         }
         else {incorrect++;
         }
             console.log ("user didn't answer");
-    
-
-        // }
-        // if (choices === answers){     
-        // correct++;
-        // }
-        // else if (choices != answers){
-        // incorrect++;
-        // }
-        // else {
-        // unanswered++;
-        // }
-        // }
     }
 }
+
 
 function results(){
 checkQuestions();
@@ -127,4 +132,33 @@ $('.game').hide();
 $('.results').append("<p>Pika Pika: " + correct + "</p>");
 $('.results').append("<p>Train Harder: " + incorrect + "</p>");
 $('.results').append("<p>Are you JigglyPuff?: " + unanswered + "</p>");
+$('#retry').show();
+}
+
+$('#retry').on('click', function(){
+    $('.start').show();
+    $('.game').hide();
+    $('.results').hide();
+    $('#retry').hide();
+    ResetGlobalVariables();
+    });
+
+function ResetGlobalVariables(){
+    timer = 10;
+    timerOn = false;
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    trivia.choices = {
+        q1: 0,
+            q2: 0,
+            q3: 0,
+            q4: 0,
+            q5: 0,
+            q6: 0,
+            q7: 0,
+            q8: 0, 
+            q9: 0,
+            q10: 0,
+    }
 }
