561f4aa878152ae524cb8cfd88b241f5cf896740
KAT <katreadwell1@gmail.com>
2020-03-14 21:45:21 -0700

    fixed trivial alingment issue in js that I forgot to correct earlier

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -147,14 +147,14 @@ function ResetGlobalVariables(){
     unanswered = 0;
     trivia.choices = {
         q1: 0,
-            q2: 0,
-            q3: 0,
-            q4: 0,
-            q5: 0,
-            q6: 0,
-            q7: 0,
-            q8: 0, 
-            q9: 0,
-            q10: 0,
+        q2: 0,
+        q3: 0,
+        q4: 0,
+        q5: 0,
+        q6: 0,
+        q7: 0,
+        q8: 0, 
+        q9: 0,
+        q10: 0,
     }
 }
